bbeb76b5ddbed032e7b635e9e4eb7cb38770fc79
Guilherme Sartori <gui.sartori96@gmail.com>
2020-07-09 03:21:35 -0300

    página de tópicos primária

--- App/Views/topicos/index.php
+++ App/Views/topicos/index.php
@@ -1,5 +1,67 @@
+<style>
+    .topicos {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
+
+    .topicos li {
+        height: 40px;
+        border: #444 1px solid;
+        background: #dadada;
+        border-radius: 2px;
+        display: flex !important;
+        padding: 8px !important;
+        margin: 4px;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+    }
+
+    .topicos li.selected {
+        background: #728062;
+        border: green 1px solid;
+        color: white;
+    }
+</style>
+
 <body>
 
+    <script>
+        let countSelectedTopicos = 0
+
+        function toggleTopicoInteresse(element, idTopico) {
+            const idUsuario = $("#idUsuario").val();
+            if ($(element).hasClass('selected')) {
+                $(element).removeClass('selected')
+                $.ajax({
+                    url: '/topico/remove',
+                    method: 'POST',
+                    data: {
+                        idUsuario,
+                        idTopico
+                    },
+                });
+                countSelectedTopicos--
+            } else {
+                $(element).addClass('selected')
+                $.ajax({
+                    url: '/topico/add',
+                    method: 'POST',
+                    data: {
+                        idUsuario,
+                        idTopico
+                    },
+                });
+                countSelectedTopicos++
+            }
+            if (countSelectedTopicos >= 3) {
+                $('#btContinue').attr("disabled", false);
+            } else {
+                $('#btContinue').attr("disabled", true);
+            }
+        }
+    </script>
     <main>
         <div class="main-section">
             <div class="container">
@@ -7,11 +69,13 @@
                     <div class="row">
                         <div class="user-profile-ov">
                             <h3>Tópicos de interesse</h3>
+                            <p>Selecione pelo menos 3 tópicos de interesse</p>
                             <ul class="topicos">
                                 <?php foreach ($aViewVar['topicos'] as $topico) { ?>
                                     <li class="<?php echo App\Lib\Util::verificaInteresse(App\Lib\Auth::usuario()->id, $topico['id']) ? 'selected' : ''; ?>" onclick="javascript: toggleTopicoInteresse(this, <?php echo $topico['id']; ?>)"><?php echo $topico['nome']; ?></li>
                                 <?php } ?>
                             </ul>
+                            <button class="btn btn-editar btn-sm" disabled onclick="javascript: window.location.href= '/principal'">Continue</button>
                         </div>
                     </div>
                 </div>
